7d4411941a55c9e9a1290f67b6da72dda4ae8191
Aleksandar Stojakovic <office@most-connect.com>
2025-11-04 16:03:45 +0100

    Fix setup

--- docker-prod/bin/setup.sh
+++ docker-prod/bin/setup.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-# 1) ins Projekt-Verzeichnis wechseln (falls Du das Script von woanders aufrufst)
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-# 2) .env laden
-if [ -f ../.env ]; then
-  # Export aller Variablen aus .env
-  set -o allexport
-  source ../.env
-  set +o allexport
-else
-  echo "⚠️  .env nicht gefunden, breche ab."
-  exit 1
-fi
-
-docker exec "$PHP_CONTAINER_NAME" dos2unix bin/console
-docker exec "$PHP_CONTAINER_NAME" composer install --no-dev --optimize-autoloader
+docker exec brux_system_php dos2unix bin/console
+docker exec brux_system_php composer install --no-dev --optimize-autoloader
